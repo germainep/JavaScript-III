fd0244957983146d667f71d9474ba8a9f95ed9c4
Germaine Pannell <me@germainep.com>
2019-08-13 23:57:28 -0400

    defined the types of 'this' binding

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,31 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. window binding
+* 2. implicit binding
+* 3.  new binding
+* 4.  explicit binding
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
+//this in the global scope refers the the window.
 
 // code example for Window Binding
 
+
 // Principle 2
+// Whenever an object is calling a function the calling object is this.
 
 // code example for Implicit Binding
 
 // Principle 3
+// When using the new keyword this refers to the instance of the object created
 
 // code example for New Binding
 
 // Principle 4
+// When using call and apply this is defined explicitly
 
 // code example for Explicit Binding
